ad40956e3928d0aaa24bd6b428d30ad5fa5fb3c2
Hiki <aneureka2@gmail.com>
2019-05-11 17:03:20 +0800

    minDistance

--- src/Editor.java
+++ src/Editor.java
@@ -82,7 +82,7 @@ public class Editor extends Worker {
     }
 
     private List<String> splitText(String data) {
-        Pattern punctuationPattern = Pattern.compile("[，。？！；]");
+        Pattern punctuationPattern = Pattern.compile("[，。？！＠＃￥％……＆（）]");
         Matcher m = punctuationPattern.matcher(data);
         List<String> sentences = new ArrayList<>();
         int curIndex = 0;
@@ -257,7 +257,7 @@ public class Editor extends Worker {
      */
     public double minDistance(String title1, String title2) {
         int editDistance = getEditDistance(title1, title2);
-        int similarity = (1 - editDistance / Math.max(title1.length(), title2.length())) * 100;
+        double similarity = (1 - (double) editDistance / Math.max(title1.length(), title2.length())) * 100;
         DecimalFormat df = new DecimalFormat("#.##");
         return Double.valueOf(df.format(similarity));
     }
@@ -265,28 +265,27 @@ public class Editor extends Worker {
     // 计算两字符串的编辑距离
     private int getEditDistance(String str1, String str2) {
 
-        int len1 = str1.length();
-        int len2 = str2.length();
-        int[][] edit = new int[len1][len2];
-        for (int i = 0; i < len1; i++) {
-            edit[i][0] = i;
-        }
-        for (int j = 0; j < len2; j++) {
-            edit[0][j] = j;
-        }
-
-        for (int i = 0; i < len1; i++) {
-            for (int j = 0; j < len2; j++) {
-                edit[i][j] = Integer.min(edit[i - 1][j] + 1, edit[i][j - 1] + 1);
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    edit[i][j] = Integer.min(edit[i][j], edit[i - 1][j - 1]);
+        int[] dp = new int[str2.length() + 1];
+        for (int i = 0; i <= str1.length(); i++) {
+            int last = i;
+            for (int j = 0; j <= str2.length(); j++) {
+                if (i == 0) {
+                    dp[j] = j;
                 } else {
-                    edit[i][j] = Integer.min(edit[i][j], edit[i - 1][j - 1] + 1);
+                    if (j > 0) {
+                        int distance = dp[j - 1];
+                        if (str1.charAt(i-1) != str2.charAt(j-1))
+                            distance = Math.min(Math.min(distance, last),
+                                    dp[j]) + 1;
+                        dp[j - 1] = last;
+                        last = distance;
+                    }
                 }
             }
+            if (i > 0)
+                dp[str2.length()] = last;
         }
-
-        return edit[len1 - 1][len2 - 1];
+        return dp[str2.length()];
     }
 
     private class ValueComparator implements Comparator<Map.Entry<String, Integer>> {
